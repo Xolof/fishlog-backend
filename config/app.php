740d576cdb4af69a9279bf469ebb4ce66ebe9ef4
Olof Johansson <olaf.sebastian.johansson@gmail.com>
2022-02-28 21:05:06 +0100

    Install Intervention/Image

--- config/app.php
+++ config/app.php
@@ -175,6 +175,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        'Intervention\Image\ImageServiceProvider',
     ],
 
     /*
@@ -230,6 +231,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
         'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
+        'Image' => 'Intervention\Image\Facades\Image',
     ],
 
 ];
